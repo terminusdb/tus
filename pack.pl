bc46c346eeca297f4afeee70dc79aad212e51ef3
Gavin Mendel-Gleason <gavin@terminusdb.com>
2021-02-01 00:08:41 +0100

    Update pack version

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name('tus').
-version('0.0.5').
+version('0.0.6').
 keywords(['tus', 'file', 'upload', 'http']).
 title('TUS file transport protocol in prolog').
 home( 'https://github.com/terminusdb/tus' ).
