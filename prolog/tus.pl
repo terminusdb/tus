18e4006fccda585cd8a692051b3fda19f4c1dab8
Robin de Rooij <rderooij685@gmail.com>
2022-01-31 15:01:10 +0100

    add full stop

--- prolog/tus.pl
+++ prolog/tus.pl
@@ -57,7 +57,7 @@
 :- use_module(library(lists)).
 :- use_module(library(option)).
 :- use_module(library(readutil)).
-:- use_module(library(plunit))
+:- use_module(library(plunit)).
 
 /* Options */
 :- meta_predicate valid_options(+, 1).
