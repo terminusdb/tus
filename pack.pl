0affa99d345550e08acc08c9952e6b10797606c9
Sean Leather <sean.leather@gmail.com>
2022-05-18 20:59:01 +0200

    Update to v0.0.10

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name('tus').
-version('0.0.9').
+version('0.0.10').
 keywords(['tus', 'file', 'upload', 'http']).
 title('TUS file transport protocol in prolog').
 home( 'https://github.com/terminusdb/tus' ).
