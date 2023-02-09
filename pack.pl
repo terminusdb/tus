99fcefc8230590bc9c4e7bc764c603e7986c2b44
Robin de Rooij <rderooij685@gmail.com>
2023-02-09 13:00:34 +0100

    increase version

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name('tus').
-version('0.0.13').
+version('0.0.14').
 keywords(['tus', 'file', 'upload', 'http']).
 title('TUS file transport protocol in prolog').
 home( 'https://github.com/terminusdb/tus' ).
