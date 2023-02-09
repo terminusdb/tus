cd476ec60799bf51e80f0f6ce4a82d0e02f8edce
Gavin Mendel-Gleason <gavin@terminusdb.com>
2023-02-09 11:29:56 +0100

    Removing tabs

--- prolog/tus.pl
+++ prolog/tus.pl
@@ -784,7 +784,7 @@ tus_create(Endpoint, File, Length, Resource, Reply_Header, Tus_Options, Options)
                  request_header('Upload-Length'=Length),
                  request_header('Upload-Metadata'=Metadata),
                  request_header('Tus-Resumable'='1.0.0'),
-		         request_header('Content-Length'=0),
+                 request_header('Content-Length'=0),
                  content_length(0),
                  reply_header(Reply_Header),
                  status_code(Code)
